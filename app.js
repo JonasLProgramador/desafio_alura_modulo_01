aa4fb5d625921f7d56d3c58c969304dee4fabf31
Jonas Leite <jonas.programador01@gmail.com>
2025-02-15 01:27:07 -0300

    :sparkles: make the first simple challenge :heavy_check_mark:

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let dificuldade = 100
+let numeroSecreto = parseInt(Math.random() * `${dificuldade}` + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
+
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt('Escolha um número entre 1 e'+ `${dificuldade}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
